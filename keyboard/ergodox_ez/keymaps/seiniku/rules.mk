172005af077421c3e91ec11a47ce5a333127bd49
Justin <j@keppers.org>
2019-02-28 15:45:20 -0600

    add seiniku ergodox config

---
+++ keyboard/ergodox_ez/keymaps/seiniku/rules.mk
@@ -0,0 +1,7 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+COMMAND_ENABLE = no
+AUTO_SHIFT_ENABLE = yes
+SRC = matrix.c
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
